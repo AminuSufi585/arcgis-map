7eee390e72075b0b572da8f36faa89e370359f73
AminuSufi585 <aminukano585@gmail.com>
2018-11-09 11:15:20 +0100

    plot on map

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
+import { WebMap, WebScene } from 'react-arcgis';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
-      <div>
-        React ArcGIS Map
+      <div style={{ width: '100vw', height: '100vh' }}>
+        <WebMap id="6627e1dd5f594160ac60f9dfc411673f" />
+        <WebScene id="f8aa0c25485a40a1ada1e4b600522681" />
       </div>
     );
   }
 }
-
-export default App;
