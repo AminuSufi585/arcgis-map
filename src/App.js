9e0d65a0dbe70c3566ccc571b7863accee636c28
AminuSufi585 <aminukano585@gmail.com>
2018-11-09 07:05:37 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        React ArcGIS Map
+      </div>
+    );
+  }
+}
+
+export default App;
